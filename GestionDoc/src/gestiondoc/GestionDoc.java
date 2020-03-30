444ed9a5945acdfd2c6a0346a6e2181560ad0b2a
mauricioguio <tempo08@hotmail.com>
2020-03-30 12:59:10 -0500

    mensajes de vienvenida

--- GestionDoc/src/gestiondoc/GestionDoc.java
+++ GestionDoc/src/gestiondoc/GestionDoc.java
@@ -15,6 +15,8 @@ public class GestionDoc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.print("hola");
+        System.out.print("mundo");
         // TODO code application logic here
     }
     
