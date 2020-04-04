847f538a10c76f72b273107960f9a60a4c6f3b9a
mauricioguio <tempo08@hotmail.com>
2020-04-04 13:21:53 -0500

    cargar demas archivos

--- GestionDoc/src/gestiondoc/Inicio.java
+++ GestionDoc/src/gestiondoc/Inicio.java
@@ -5,10 +5,10 @@
  */
 package gestiondoc;
 
-/**
- *
- * @author TECNOLOGIA
- */
+import AppPackage.AnimationClass;
+import javax.swing.JOptionPane;
+
+
 public class Inicio extends javax.swing.JFrame {
 
     /**
@@ -29,55 +29,112 @@ public class Inicio extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        jBconfiguraciones = new javax.swing.JButton();
+        jBgenerar = new javax.swing.JButton();
+        jBradicar = new javax.swing.JButton();
+        jBexterna = new javax.swing.JButton();
+        jBvisualizar = new javax.swing.JButton();
+        jLcerrar = new javax.swing.JLabel();
+        jLminimizar = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos configuraciones.png"))); // NOI18N
-        jButton1.setBorder(null);
-        jButton1.setBorderPainted(false);
-        jButton1.setContentAreaFilled(false);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 500, -1, -1));
+        jBconfiguraciones.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos configuraciones.png"))); // NOI18N
+        jBconfiguraciones.setBorder(null);
+        jBconfiguraciones.setBorderPainted(false);
+        jBconfiguraciones.setContentAreaFilled(false);
+        jBconfiguraciones.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        getContentPane().add(jBconfiguraciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 500, -1, -1));
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono generar documento.png"))); // NOI18N
-        jButton2.setBorder(null);
-        jButton2.setContentAreaFilled(false);
-        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 360, -1, -1));
+        jBgenerar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono generar documento.png"))); // NOI18N
+        jBgenerar.setBorder(null);
+        jBgenerar.setContentAreaFilled(false);
+        jBgenerar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        getContentPane().add(jBgenerar, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 360, -1, -1));
 
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos radicar documento.png"))); // NOI18N
-        jButton4.setBorder(null);
-        jButton4.setBorderPainted(false);
-        jButton4.setContentAreaFilled(false);
-        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 360, -1, -1));
+        jBradicar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos radicar documento.png"))); // NOI18N
+        jBradicar.setBorder(null);
+        jBradicar.setBorderPainted(false);
+        jBradicar.setContentAreaFilled(false);
+        jBradicar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jBradicar.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono radicar on.png"))); // NOI18N
+        jBradicar.setRolloverSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono radicar on.png"))); // NOI18N
+        jBradicar.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono radicar on.png"))); // NOI18N
+        jBradicar.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                jBradicarMouseMoved(evt);
+            }
+        });
+        jBradicar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jBradicarMouseExited(evt);
+            }
+        });
+        getContentPane().add(jBradicar, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 360, -1, -1));
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos documentacion externa.png"))); // NOI18N
-        jButton3.setBorder(null);
-        jButton3.setBorderPainted(false);
-        jButton3.setContentAreaFilled(false);
-        jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 360, -1, -1));
+        jBexterna.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos documentacion externa.png"))); // NOI18N
+        jBexterna.setBorder(null);
+        jBexterna.setBorderPainted(false);
+        jBexterna.setContentAreaFilled(false);
+        jBexterna.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        getContentPane().add(jBexterna, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 360, -1, -1));
 
-        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos visualizar factura.png"))); // NOI18N
-        jButton5.setBorder(null);
-        jButton5.setBorderPainted(false);
-        jButton5.setContentAreaFilled(false);
-        jButton5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 500, -1, -1));
+        jBvisualizar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/iconos visualizar factura.png"))); // NOI18N
+        jBvisualizar.setBorder(null);
+        jBvisualizar.setBorderPainted(false);
+        jBvisualizar.setContentAreaFilled(false);
+        jBvisualizar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jBvisualizar.setRolloverIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono visualizar on.png"))); // NOI18N
+        jBvisualizar.setRolloverSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono visualizar on.png"))); // NOI18N
+        jBvisualizar.setSelectedIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icono visualizar on.png"))); // NOI18N
+        getContentPane().add(jBvisualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 500, 100, 110));
+
+        jLcerrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cerrar2.png"))); // NOI18N
+        jLcerrar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLcerrar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLcerrarMouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLcerrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(738, 2, -1, -1));
+
+        jLminimizar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/minimizar2.png"))); // NOI18N
+        jLminimizar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jLminimizar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLminimizarMouseClicked(evt);
+            }
+        });
+        getContentPane().add(jLminimizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(705, 2, -1, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Fundacion mira tu salud.png"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jLcerrarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLcerrarMouseClicked
+        int dialog = JOptionPane.YES_NO_OPTION;
+        int result = JOptionPane.showConfirmDialog(null, "Desea salir de esta ventana?","Exit",dialog);
+        if(result == 0){
+            System.exit(0);
+        }
+    }//GEN-LAST:event_jLcerrarMouseClicked
+
+    private void jLminimizarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLminimizarMouseClicked
+        this.setState(Inicio.ICONIFIED);        // TODO add your handling code here:
+    }//GEN-LAST:event_jLminimizarMouseClicked
+
+    private void jBradicarMouseMoved(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jBradicarMouseMoved
+        
+    }//GEN-LAST:event_jBradicarMouseMoved
+
+    private void jBradicarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jBradicarMouseExited
+       
+    }//GEN-LAST:event_jBradicarMouseExited
 
     /**
      * @param args the command line arguments
@@ -115,11 +172,13 @@ public class Inicio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jBconfiguraciones;
+    private javax.swing.JButton jBexterna;
+    private javax.swing.JButton jBgenerar;
+    private javax.swing.JButton jBradicar;
+    private javax.swing.JButton jBvisualizar;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLcerrar;
+    private javax.swing.JLabel jLminimizar;
     // End of variables declaration//GEN-END:variables
 }
